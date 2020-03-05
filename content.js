09359d5d1bb68d4ebd0aff12f01265259e81c68e
Philipp Rapp <philipprapp@Red5.local>
2020-03-05 14:00:43 +0100

    First Push

---
+++ content.js
@@ -0,0 +1,98 @@
+function keywordsHighlighter(remove) {
+  var occurrences = 0;
+  var keyword_occurrences = [];
+  // var scrollingElement = document.scrollingElement || document.body;
+  // scrollingElement.scrollTop = scrollingElement.scrollHeight;
+  // setTimeout(function() {
+  //   var scrollingElement = document.scrollingElement || document.body;
+  //   scrollingElement.scrollTop = 0;
+  // }, 3);
+
+  // Based on "highlight: JavaScript text higlighting jQuery plugin" by Johann Burkard.
+  // http://johannburkard.de/blog/programming/javascript/highlight-javascript-text-higlighting-jquery-plugin.html
+  // MIT license.
+  function highlight(node, pos, keyword) {
+    keyword_occurrences.push(keyword);
+    var span = document.createElement("span");
+    span.className = "highlighted";
+    span.style.color = "#292524";
+    span.style.fontWeight = "900";
+    span.style.backgroundColor = "#fee603";
+
+    var highlighted = node.splitText(pos);
+    /*var afterHighlighted = */ highlighted.splitText(keyword.length);
+    var highlightedClone = highlighted.cloneNode(true);
+
+    span.appendChild(highlightedClone);
+    highlighted.parentNode.replaceChild(span, highlighted);
+
+    occurrences++;
+  }
+
+  function addHighlights(node, keywords) {
+    var skip = 0;
+
+    var i;
+    if (3 == node.nodeType) {
+      for (i = 0; i < keywords.length; i++) {
+        var keyword = keywords[i].toLowerCase();
+        var pos = node.data.toLowerCase().indexOf(keyword);
+        if (0 <= pos) {
+          highlight(node, pos, keyword);
+          skip = 1;
+        }
+      }
+    } else if (
+      1 == node.nodeType &&
+      !/(script|style|textarea)/i.test(node.tagName) &&
+      node.childNodes
+    ) {
+      for (i = 0; i < node.childNodes.length; i++) {
+        i += addHighlights(node.childNodes[i], keywords);
+      }
+    }
+
+    return skip;
+  }
+
+  function removeHighlights(node) {
+    var span;
+    while ((span = node.querySelector("span.highlighted"))) {
+      span.outerHTML = span.innerHTML;
+    }
+
+    occurrences = 0;
+  }
+
+  if (remove) {
+    removeHighlights(document.body);
+  }
+  var buzzwords =
+    "New Work, NewWork, Scrum, Design Thinking, Purpose, Agilität, Agil, Obstkorb, gesellschaftlicher Mehrwert, Jobsharing, New Leadership, menschenzentriert, digital, menschzentriert, lego, serious play, tischtennis, table tennis, tischkicker, yolo, kultur, culture, cultural, work-life-balance, ai, identität, wert, vuca, change, enable, enablement, potential, design, mindset, big data, freiheit, freeedom, performance, arbeit 4.0, digitalisierung, management 3.0, lernende organisation, netzwerkorganisation, schwarm, bällebad, lean, bottom up";
+  var keywords = buzzwords.split(",");
+  addHighlights(document.body, keywords);
+  var total_number_words = document.body.innerText
+    .split(/\s/)
+    .filter(function(txt) {
+      return /\S/.test(txt);
+    }).length;
+
+  chrome.runtime.sendMessage({
+    message: "showOccurrences",
+    occurrences: occurrences,
+    keyword_occurrences: keyword_occurrences,
+    total_number_words: total_number_words
+  });
+}
+
+console.log(window.location.href);
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  keywordsHighlighter(false);
+
+  console.log(
+    document.body.innerText.split(/\s/).filter(function(txt) {
+      return /\S/.test(txt);
+    })
+  );
+});
---
+++ popup.js
@@ -0,0 +1,77 @@
+document.addEventListener(
+  "DOMContentLoaded",
+
+  function() {
+    function animateValue(id, start, end, duration) {
+      var range = end - start;
+      var current = start;
+      var increment = end > start ? 1 : -1;
+      var stepTime = Math.abs(Math.floor(duration / range));
+      var obj = document.getElementById(id);
+      var timer = setInterval(function() {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+          clearInterval(timer);
+        } else if (end == 0) {
+          obj.innerHTML = 0;
+          clearInterval(timer);
+        }
+      }, stepTime);
+    }
+    document.querySelector("button").addEventListener("click", onclick, false);
+    function onclick() {
+      document.getElementById("loading_frame").style.display = "block";
+      document.getElementById("morph_text").style.display = "flex";
+      document.getElementById("div-result").style.display = "none";
+      document.getElementById("highlight").style.display = "none";
+      chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, "getOccurrences");
+      });
+    }
+    function setCount(occurrences, keyword_occurrences, total_number_words) {
+      setTimeout(function() {
+        document.getElementById("loading_frame").style.display = "none";
+        document.getElementById("div-result").style.display = "flex";
+        document.getElementById("headline").classList.add("mark-once");
+        if (occurrences > 1) {
+          document.getElementById("wow-gif").style.display = "flex";
+          document.getElementById("total_amount").innerHTML =
+            " out of " + String(total_number_words) + " words found:";
+          document.getElementById("result-topwords").innerHTML = String(
+            keyword_occurrences
+          );
+        } else {
+          document.getElementById("nice-gif").style.display = "flex";
+          document.getElementById("total_amount").innerHTML =
+            " out of " +
+            String(total_number_words) +
+            " words found. <br /> Not bad! ✊";
+          document.getElementById("result-topwords").innerHTML = String(
+            keyword_occurrences
+          );
+        }
+
+        // document.getElementById("highlight").innerHTML = "Again";
+        document.getElementById("morph_text").style.display = "none";
+        // document.getElementById("result-amount").innerHTML = String(
+        //   occurrences
+        // );
+        animateValue("result-amount", 0, occurrences, 5000);
+      }, 4000);
+    }
+
+    chrome.runtime.onMessage.addListener(function(request) {
+      if ("showOccurrences" == request.message) {
+        var occurrences = request.occurrences;
+        var keyword_occurrences = request.keyword_occurrences;
+        var total_number_words = request.total_number_words;
+        var average_buzzwords = (100 * occurrences) / total_number_words;
+        if (occurrences == 0) {
+        }
+        setCount(occurrences, keyword_occurrences, total_number_words);
+      }
+    });
+  },
+  false
+);
